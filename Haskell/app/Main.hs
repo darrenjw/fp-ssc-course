cc0bcab86471501c5a0865417d06cd91d5f2dc4b
Darren Wilkinson <darrenjwilkinson@btinternet.com>
2023-01-14 19:15:20 +0000

    starting to create the haskell example

--- Haskell/app/Main.hs
+++ Haskell/app/Main.hs
@@ -1,6 +1,59 @@
+{-# LANGUAGE ConstraintKinds, DataKinds, FlexibleContexts, GADTs,
+ OverloadedStrings, PatternSynonyms, QuasiQuotes,
+ ScopedTypeVariables, TemplateHaskell, TypeOperators, TypeApplications,
+ ViewPatterns #-}
+
 module Main (main) where
 
 import Lib
+import Frames
+import Frames.TH (rowGen, RowGen(..))
+import Pipes hiding (Proxy)
+import Numeric.LinearAlgebra
+
+
+-- template Haskell to create the Person type, and personParser
+tableTypes' (rowGen "../pima.data")
+            { rowTypeName = "Person"
+            , columnNames = [ "npreg", "glu", "bp"
+                            , "skin", "bmi", "ped", "age", "yy" ]
+            , separator = " " }
+
+-- create a data stream
+dataStream :: MonadSafe m => Producer Person m ()
+dataStream = readTableOpt personParser "../pima.data"
+
+-- load full dataset
+loadData :: IO (Frame Person)
+loadData = inCoreAoS dataStream
+
+-- create rows of covariate matrix
+rec2l :: Person -> [Double]
+rec2l r = [1.0, fromIntegral $ rgetField @Npreg r, fromIntegral $ rgetField @Glu r,
+           fromIntegral $ rgetField @Bp r, fromIntegral $ rgetField @Skin r,
+            rgetField @Bmi r, rgetField @Ped r, fromIntegral $ rgetField @Age r]
+
+-- sum an hmatrix Vector
+vsum :: Vector Double -> Double
+vsum v = (konst 1 (size v) :: Vector Double) <.> v
+
+-- log-likelihood
+ll :: Matrix Double -> Vector Double -> Vector Double -> Double
+ll x y b = (negate) (vsum (cmap log (
+                              (scalar 1) + (cmap exp (cmap (negate) (
+                                                         (((scalar 2) * y) - (scalar 1)) * (x #> b)
+                                                         )
+                                                     )))))
+
+-- gradient
+gll :: Matrix Double -> Vector Double -> Vector Double -> Vector Double
+gll x y b = (tr x) #> (y - (scalar 1)/((scalar 1) + (cmap exp (-x #> b))))
+
+
+
 
 main :: IO ()
-main = someFunc
+main = do
+  putStrLn "Gradient ascent for a log likelihood"
+  dat <- loadData
+  putStrLn "Goodbye."

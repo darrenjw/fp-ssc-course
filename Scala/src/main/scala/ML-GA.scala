bf1495915161489bea6efec4f48756cb9ae77d64
Darren Wilkinson <darrenjwilkinson@btinternet.com>
2023-01-14 17:20:33 +0000

    gradient ascent working ok

--- Scala/src/main/scala/ML-GA.scala
+++ Scala/src/main/scala/ML-GA.scala
@@ -9,6 +9,7 @@ of a logistic regression model, applied to the Pima data
 import breeze.linalg.*
 import breeze.numerics.*
 import smile.data.pimpDataFrame
+import annotation.tailrec
 
 type DVD = DenseVector[Double]
 
@@ -37,7 +38,29 @@ object GradientAscent:
     def gll(beta: DVD): DVD =
       (X.t)*(y - ones/:/(ones + exp(-X*beta)))
 
+    println("Now define a functions for gradient ascent")
+    def oneStep(learningRate: Double)(b0: DVD): DVD =
+      b0 + learningRate*gll(b0)
+    def ascend(step: DVD => DVD, init: DVD, maxIts: Int = 10000,
+        tol: Double = 1e-8, verb: Boolean = true): DVD =
+      @tailrec def go(b0: DVD, ll0: Double, itsLeft: Int): DVD =
+        if (verb)
+          println(s"$itsLeft : $ll0")
+        val b1 = step(b0)
+        val ll1 = ll(b1)
+        if ((math.abs(ll0 - ll1) < tol)|(itsLeft < 1))
+          b1
+        else
+          go(b1, ll1, itsLeft - 1)
+      go(init, ll(init), maxIts)
 
-
+    println("Now run a simple gradient ascent algorithm")
+    // Better choose a reasonable init as gradient ascent is terrible...
+    val init = DenseVector(-9.8, 0.1, 0, 0, 0, 0, 1.8, 0)
+    val opt = ascend(oneStep(1e-6), init)
+    println("Inits: " + init)
+    println("Init ll: " + ll(init))
+    println("Opt: " + opt)
+    println("Opt ll: " + ll(opt))
     println("Goodbye.")
 

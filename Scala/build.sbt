3202b94085d9fd23897a61e58129e334f3eb1920
Darren Wilkinson <darrenjwilkinson@btinternet.com>
2023-01-14 16:14:36 +0000

    starting on example

--- Scala/build.sbt
+++ Scala/build.sbt
@@ -17,6 +17,8 @@ libraryDependencies  ++= Seq(
   "org.scalameta" %% "munit" % "0.7.29" % Test,
   "org.scalameta" %% "munit-scalacheck" % "0.7.29" % Test,
   "org.typelevel" %% "discipline-munit" % "1.0.9" % Test,
+  "org.scalanlp" %% "breeze" % "2.1.0",
+  ("com.github.haifengl" %% "smile-scala" % "3.0.0").cross(CrossVersion.for3Use2_13),
   "org.typelevel" %% "cats-core" % "2.8.0",
   "org.typelevel" %% "cats-free" % "2.8.0",
   "org.typelevel" %% "cats-laws" % "2.8.0",
--- Scala/src/main/scala/ML-GA.scala
+++ Scala/src/main/scala/ML-GA.scala
@@ -1,15 +1,43 @@
 /*
 ML-GA.scala
-Stub for Scala Cats code
+
+Simple gradient ascent algorithm for maximum likelihood estimation
+of a logistic regression model, applied to the Pima data
+
 */
 
-import cats.*
-import cats.implicits.*
-import cats.effect.{IO, IOApp}
+import breeze.linalg.*
+import breeze.numerics.*
+import smile.data.pimpDataFrame
 
-object CatsApp extends IOApp.Simple:
+type DVD = DenseVector[Double]
 
-  val l = List(1,2) |+| List(3,4)
+object GradientAscent:
 
-  def run = IO{ println(l) }
+
+  @main def run() =
+    println("First read and process the data")
+    val df = smile.read.csv("../pima.data", delimiter=" ", header=false)
+    print(df)
+    val y = DenseVector(df.select("V8").
+      map(_(0).asInstanceOf[String]).
+      map(s => if (s == "Yes") 1.0 else 0.0).toArray)
+    println(y)
+    val x = DenseMatrix(df.drop("V8").toMatrix.toArray:_*)
+    println(x)
+    val ones = DenseVector.ones[Double](x.rows)
+    val X = DenseMatrix.horzcat(ones.toDenseMatrix.t, x)
+    println(X)
+    val p = X.cols
+    println(p)
+
+    println("Now define log likelihood and gradient")
+    def ll(beta: DVD): Double =
+      sum(-log(ones + exp(-1.0*(2.0*y - ones)*:*(X * beta))))
+    def gll(beta: DVD): DVD =
+      (X.t)*(y - ones/:/(ones + exp(-X*beta)))
+
+
+
+    println("Goodbye.")
 

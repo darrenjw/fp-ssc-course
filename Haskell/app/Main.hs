98ba5d38f04b0be8211d694ac5697ee9fd209587
Darren Wilkinson <darrenjwilkinson@btinternet.com>
2023-01-15 15:35:03 +0000

    working implementation

--- Haskell/app/Main.hs
+++ Haskell/app/Main.hs
@@ -5,8 +5,6 @@
 
 module Main (main) where
 
---import GHC.Prim
---import Control.Monad
 import Frames
 import Frames.TH (rowGen, RowGen(..))
 import Pipes hiding (Proxy)
@@ -51,12 +49,26 @@ ll x y b = (negate) (vsum (cmap log (
 gll :: Matrix Double -> Vector Double -> Vector Double -> Vector Double
 gll x y b = (tr x) #> (y - (scalar 1)/((scalar 1) + (cmap exp (-x #> b))))
 
+-- one step of gradient ascent
+oneStep :: Matrix Double -> Vector Double -> Double -> Vector Double -> Vector Double
+oneStep x y lrate b0 = b0 + (scalar lrate) * (gll x y b0)
 
-
-
+-- function for ascent
+ascend :: Matrix Double -> Vector Double -> (Vector Double -> Vector Double) ->
+  Vector Double -> Int -> Double -> Vector Double
+ascend x y step init maxIts tol =
+    go init maxIts
+  where 
+    go b0 itsLeft =
+      let b1 = step b0
+          d = norm_2 $ b1 - b0
+          in if ((d < tol)||(itsLeft < 1)) then b1
+                else go b1 (itsLeft - 1)
+    
 main :: IO ()
 main = do
   putStrLn "Gradient ascent for a log likelihood"
+  putStrLn "First load and process the data..."
   dat <- loadData
   let yl = (\x -> if x then 1.0 else 0.0) <$> F.toList (view yy <$> dat)
   let xl = rec2l <$> F.toList dat
@@ -65,6 +77,14 @@ main = do
   print y
   let x = fromLists xl 
   disp 2 x
+  putStrLn "Now run the gradient ascent"
+  -- choose reasonable init, since gradient ascent is terrible...
+  let init = fromList [-9.8, 0.1, 0, 0, 0, 0, 1.8, 0] :: Vector Double
+  print init
+  print $ ll x y init
+  let opt = ascend x y (oneStep x y 1.0e-6) init 10000 0.0001
+  print opt
+  print $ ll x y opt
   putStrLn "Goodbye."
 
 
